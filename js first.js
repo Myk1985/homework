d8a3c4d92c9588f8b888fec13040a61aae17720d
Myk1985 <nikolay28v85@gmail.com>
2023-05-02 00:34:07 +0300

    Initial commit

---
+++ js first.js
@@ -0,0 +1,46 @@
+let a = `hello`
+console.log(a)
+let b = `owu`
+console.log(b)
+let c = `com`
+console.log(c)
+let d = `ua`
+console.log(d)
+let e = 1
+console.log(e)
+let f = 10
+console.log(f)
+let g = -999
+console.log(g)
+let h = 3.14
+console.log(h)
+let j = 16
+console.log(j)
+let k = true
+console.log(k)
+let l = false
+console.log(l)
+
+let fN = 'Васильєв'
+let mN = 'Микола'
+let lN =    'Олегович'
+console.log(fN+mN+lN)
+
+
+
+
+
+let r = 100
+console.log(typeof r)
+let n = `100`
+console.log(typeof n)
+let m = true
+console.log(typeof m)
+let name = prompt(`Васильєв`)
+let midd = prompt('Микола')
+let last = prompt('Олегович')
+console.log(name)
+console.log(midd)
+console.log(last)
+
+
